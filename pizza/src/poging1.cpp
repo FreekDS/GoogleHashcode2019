5f0876de6cc26484a0847a7f0dad65eba2eec8ae
thibautvangoethem <thibaut.vangoethem@student.uantwerpen.be>
2019-02-18 12:06:44 +0100

    fixed probleem in pogin1rijen en heb pogin1kolommen gemaakt

--- pizza/src/poging1.cpp
+++ pizza/src/poging1.cpp
@@ -34,11 +34,14 @@ vector<Slice> poging1Rijen(Pizza& p)
             by++;
             cx = bx+1;
             cy = by;
+            if (by>p.colNum-1) {
+                break;
+            }
         }
         Position p1 = Position(bx, by);
         Position p2 = Position(cx, cy);
         if (checkEnoughIngredients(p, p1, p2)) {
-            if (bx-cx<=p.minIngredients) {
+            if (cx-bx<=p.sliceSize) {
                 auto pair = make_pair(p1, p2);
                 solution.push_back(pair);
                 bx = cx+1;
@@ -59,11 +62,52 @@ vector<Slice> poging1Rijen(Pizza& p)
 
 vector<Slice> poging1kolommen(Pizza& p)
 {
+    vector<Slice> solution;
+    unsigned int bx = 0;
+    unsigned int by = 0;
+    unsigned int cx = 0;
+    unsigned int cy = 0;
+    while (true) {
+        cy++;
+        if (cy>p.colNum-1) {
+            by = 0;
+            bx++;
+            cy = by+1;
+            cx = bx;
+            if (bx>p.rowNum-1) {
+                break;
+            }
+        }
+        Position p1 = Position(bx, by);
+        Position p2 = Position(cx, cy);
+        if (checkEnoughIngredients(p, p1, p2)) {
+            if (cy-by<=p.sliceSize) {
+                auto pair = make_pair(p1, p2);
+                solution.push_back(pair);
+                by = cy+1;
+                bx = cx;
+            }
+            else {
+                by++;
+                cy--;
+            }
+        }
+        if (bx>p.rowNum-1) {
+            break;
+        }
 
+    }
+    return solution;
 }
 
 vector<Slice> poging1(Pizza& p)
 {
-    return poging1Rijen(p);
+    auto p1r=poging1Rijen(p);
+    auto p1k=poging1kolommen(p);
+    p.slices=p1r;
+    int sp1r=p.calculateScore();
+    p.slices=p1k;
+    int sp1k=p.calculateScore();
+    return (sp1r>sp1k)? p1r:p1k;
 }
 
